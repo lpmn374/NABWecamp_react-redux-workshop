4bcd46d36d254c7a39540fb464407aecee6d2abb
Sapphira Le <lpmn374@gmail.com>
2026-06-14 18:55:49 +0700

    Submit 04-useMemo exercise

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,43 @@
-import { useState, useEffect } from 'react'
-import type { Expense } from './types/expense'
-import { STORAGE_KEY } from './constants'
-import { filterExpenses } from './utils/filterExpenses'
-import ExpenseForm from './components/ExpenseForm'
-import ExpenseList from './components/ExpenseList'
-import ExpenseSummary from './components/ExpenseSummary'
-import SearchBar from './components/SearchBar'
-import './App.css'
+import { useState, useEffect, useMemo } from "react";
+import type { Expense } from "./types/expense";
+import { STORAGE_KEY } from "./constants";
+import { filterExpenses } from "./utils/filterExpenses";
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseList from "./components/ExpenseList";
+import ExpenseSummary from "./components/ExpenseSummary";
+import SearchBar from "./components/SearchBar";
+import "./App.css";
 
 function App() {
   const [expenses, setExpenses] = useState<Expense[]>(() => {
-    const saved = localStorage.getItem(STORAGE_KEY)
-    return saved ? (JSON.parse(saved) as Expense[]) : []
-  })
+    const saved = localStorage.getItem(STORAGE_KEY);
+    return saved ? (JSON.parse(saved) as Expense[]) : [];
+  });
 
-  const [query, setQuery] = useState('')
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(expenses))
-  }, [expenses])
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(expenses));
+  }, [expenses]);
 
   // TODO: wrap in useMemo(() => filterExpenses(expenses, query), [expenses, query])
-  const filteredExpenses = filterExpenses(expenses, query)
+  const filteredExpenses = useMemo(
+    () => filterExpenses(expenses, query),
+    [expenses, query],
+  );
 
   // TODO: wrap in useMemo(() => filteredExpenses.reduce(...), [filteredExpenses])
-  const total = filteredExpenses.reduce((sum, e) => sum + e.amount, 0)
+  const total = useMemo(
+    () => filteredExpenses.reduce((sum, e) => sum + e.amount, 0),
+    [filteredExpenses],
+  );
 
-  function handleAddExpense(expense: Omit<Expense, 'id'>) {
-    setExpenses(prev => [
-      ...prev,
-      { ...expense, id: crypto.randomUUID() },
-    ])
+  function handleAddExpense(expense: Omit<Expense, "id">) {
+    setExpenses((prev) => [...prev, { ...expense, id: crypto.randomUUID() }]);
   }
 
   function handleDeleteExpense(id: string) {
-    setExpenses(prev => prev.filter(e => e.id !== id))
+    setExpenses((prev) => prev.filter((e) => e.id !== id));
   }
 
   return (
@@ -52,7 +55,7 @@ function App() {
         />
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
