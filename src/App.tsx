a039df99d2a9ec5e5a3947f45061ac6ed0aec3c3
Sapphira Le <lpmn374@gmail.com>
2026-06-14 18:35:44 +0700

    Submit 02-useEffect exercise

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import { useState } from 'react'
-import type { Expense } from './types/expense'
-import ExpenseForm from './components/ExpenseForm'
-import ExpenseList from './components/ExpenseList'
-import './App.css'
+import { useState, useEffect } from "react";
+import type { Expense } from "./types/expense";
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseList from "./components/ExpenseList";
+import "./App.css";
 
 function App() {
-  const [expenses, setExpenses] = useState<Expense[]>([])
+  const [expenses, setExpenses] = useState<Expense[]>(() => {
+    const savedExpenses = localStorage.getItem("expenses");
+    return savedExpenses ? JSON.parse(savedExpenses) : [];
+  });
 
   // TODO: load expenses from localStorage on mount
+  // useEffect(() => {
+  //   const savedExpenses = localStorage.getItem("expenses");
+  //   if (savedExpenses) {
+  //     setExpenses(JSON.parse(savedExpenses));
+  //   }
+  // }, []);
   // TODO: save expenses to localStorage whenever the list changes
+  useEffect(() => {
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }, [expenses]);
 
-  function handleAddExpense(expense: Omit<Expense, 'id'>) {
-    setExpenses(prev => [
-      ...prev,
-      { ...expense, id: crypto.randomUUID() },
-    ])
+  function handleAddExpense(expense: Omit<Expense, "id">) {
+    setExpenses((prev) => [...prev, { ...expense, id: crypto.randomUUID() }]);
   }
 
   function handleDeleteExpense(id: string) {
-    setExpenses(prev => prev.filter(e => e.id !== id))
+    setExpenses((prev) => prev.filter((e) => e.id !== id));
   }
 
   return (
@@ -34,7 +43,7 @@ function App() {
         />
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
